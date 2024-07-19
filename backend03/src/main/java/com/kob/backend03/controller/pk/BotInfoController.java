f10d5148e8f03ed12085edc96c91eb8580a034c9
shan xue <339754794@qq.com>
2024-07-20 00:39:42 +0800

    项目基本骨架搭建完成

--- backend03/src/main/java/com/kob/backend03/controller/pk/BotInfoController.java
+++ backend03/src/main/java/com/kob/backend03/controller/pk/BotInfoController.java
@@ -15,17 +15,11 @@ import java.util.Map;
 @RestController
 @RequestMapping("/pk/")
 public class BotInfoController {
-    @RequestMapping("/getBotInfo/")
-    public List<Map<String,String>>  getBotInfo(){
-        List<Map<String,String>> list = new LinkedList<>();
+    @RequestMapping("getBotInfo/")
+    public Map<String,String> getBotInfo(){
         Map<String,String> bot1 = new HashMap<>();
-        bot1.put("name","tiger");
+        bot1.put("name","apple");
         bot1.put("rating","1500");
-        Map<String,String> bot2 = new HashMap<>();
-        bot2.put("name","apple");
-        bot2.put("rating","2000");
-        list.add(bot1);
-        list.add(bot2);
-        return list;
+        return bot1;
     }
 }
