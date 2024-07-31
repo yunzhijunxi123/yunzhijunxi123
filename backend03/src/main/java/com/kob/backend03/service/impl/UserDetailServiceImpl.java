04c4543d464691c807959c6526997d1d73f19c70
shan xue <339754794@qq.com>
2024-07-31 17:48:27 +0800

    编写用户名和密码是否匹配的代码

---
+++ backend03/src/main/java/com/kob/backend03/service/impl/UserDetailServiceImpl.java
@@ -0,0 +1,34 @@
+package com.kob.backend03.service.impl;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.kob.backend03.mapper.UserMapper;
+import com.kob.backend03.pojo.User;
+import com.kob.backend03.service.impl.utils.UserDetailImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+
+/**
+ * @author shan
+ * @version 1.0
+ */
+public class UserDetailServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {  //传入username.通过传入的username返回用户名和密码
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("username",username);  //用户名相同
+        User user = userMapper.selectOne(queryWrapper);  //查询user
+        if(user==null){
+            throw new RuntimeException("用户不存在");
+        }
+        return new UserDetailImpl();
+    }
+}
---
+++ backend03/src/main/java/com/kob/backend03/service/impl/utils/UserDetailImpl.java
@@ -0,0 +1,58 @@
+package com.kob.backend03.service.impl.utils;
+
+import com.kob.backend03.pojo.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+import java.util.Collection;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+/**
+ * @author shan
+ * @version 1.0
+ */
+public class UserDetailImpl implements UserDetails {
+    private User user;
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {  //是否没有被锁定
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {  //授权是否过期
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {  //用户是否被启用
+        return true;
+    }
+}
