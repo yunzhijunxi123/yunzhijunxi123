4567d8bc5a8ea22b4a312feb196fb5eaccd775e0
shan xue <339754794@qq.com>
2024-08-29 09:02:53 +0800

    完成了对注册和登录界面的进一步设计

--- web/src/views/user/account/UserAccountLoginView.vue
+++ web/src/views/user/account/UserAccountLoginView.vue
@@ -1,5 +1,5 @@
 <template>
-    <ContentField>
+    <ContentField v-if="!$store.state.user.pulling_info">
         <div class="row justify-content-md-center">
             <div class ="col-3">
                 <form @submit.prevent="login">
@@ -33,6 +33,25 @@ import router from '../../../router/index'
             let username = ref('');
             let password = ref('');
             let error_message = ref('');
+            let show_content = ref(false);  //展示内容
+
+
+            const jwt_token = localStorage.getItem("jwt_token");
+            if(jwt_token){
+                store.commit("updateToken",jwt_token);
+                store.dispatch("getinfo",{
+                    success(){
+                        router.push({name:"home"}); //成功就跳转首页
+                        store.commit("updatePullingInfo", false);
+                    },
+                    error(){
+                        store.commit("updatePullingInfo", false);
+                    }
+                })
+            }else{
+                store.commit("updatePullingInfo", false);
+            }
+
             const login = () =>{  //如果提交就触发该函数
                 error_message.value="";
                 store.dispatch("login",{
@@ -42,9 +61,8 @@ import router from '../../../router/index'
                         store.dispatch("getinfo",{
                             success(){
                                 router.push({name:'home'});
-                                console.log(store.state.user);
                             }
-                        })//获取用户信息
+                        })  //获取用户信息
                     },
                     error(){
                         error_message.value="用户名或密码错误";
@@ -56,6 +74,7 @@ import router from '../../../router/index'
                 password,
                 error_message,
                 login,
+                show_content,
             }
         }
     }
